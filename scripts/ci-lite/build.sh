8ce5cde84c27208a60a29dbfe2b730e28606ef64
Misko Hevery <misko@hevery.com>
2016-08-29 16:51:06 -0700

    WIP

--- scripts/ci-lite/build.sh
+++ scripts/ci-lite/build.sh
@@ -12,23 +12,7 @@ cd ../..
 $(npm bin)/tsc -v
 $(npm bin)/tsc -p tools
 cp tools/@angular/tsc-wrapped/package.json dist/tools/@angular/tsc-wrapped
-node dist/tools/@angular/tsc-wrapped/src/main -p modules
-for PACKAGE in \
-  core \
-  compiler \
-  common \
-  forms \
-  platform-browser \
-  platform-browser-dynamic \
-  platform-server \
-  http \
-  router \
-  upgrade \
-  compiler-cli
-do
-  node dist/tools/@angular/tsc-wrapped/src/main -p modules/@angular/$PACKAGE
-  cp  modules/@angular/$PACKAGE/package.json dist/packages-dist/$PACKAGE
-done
+../../build.sh
 $(npm bin)/tsc -p modules/benchpress
 
 echo 'travis_fold:end:BUILD'
